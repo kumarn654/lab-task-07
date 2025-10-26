519904976007d2eff5a367ddca799318ee193aba
Nikhil Kumar <k253105@nu.edu.pk>
2025-10-26 08:23:02 -0700

    Remove commented code and clean formatting

--- q2) lab 07.cpp
+++ q2) lab 07.cpp
@@ -1,6 +1,3 @@
-//Write a program in C to read n number of values in an array and display it in reverse order
-//Input: {1,2,3,4,5,6,7,8,9}
-//Output: 9 8 7 6 5 4 3 2 1
 #include<stdio.h>
 int main(){
 
@@ -26,3 +23,4 @@ int main(){
 
 	return 0;
 }
+
